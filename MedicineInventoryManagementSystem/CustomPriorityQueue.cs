aca7f8ba4dc0ea3bece6aa481b8b9cb62f0b75c2
NidukSuraweeraArachchi <akalankasuraweeraarachchi@gmail.com>
2025-03-04 13:48:36 +0530

    Add files via upload

---
+++ MedicineInventoryManagementSystem/CustomPriorityQueue.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MedicineInventoryManagementSystem
+{
+    public class CustomPriorityQueue
+    {
+        private class Node
+        {
+            public Medicine Medicine;
+            public Node Next;
+
+            public Node(Medicine medicine)
+            {
+                Medicine = medicine;
+                Next = null;
+            }
+        }
+
+        private Node head;
+
+        public void Enqueue(Medicine medicine)
+        {
+            Node newNode = new Node(medicine);
+
+            if (head == null || head.Medicine.ExpiryDate > medicine.ExpiryDate)
+            {
+                newNode.Next = head;
+                head = newNode;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.Next != null && temp.Next.Medicine.ExpiryDate <= medicine.ExpiryDate)
+                {
+                    temp = temp.Next;
+                }
+                newNode.Next = temp.Next;
+                temp.Next = newNode;
+            }
+        }
+
+        public Medicine Peek()
+        {
+            return head?.Medicine;
+        }
+    }
+}
